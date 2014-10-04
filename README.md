50d43445ec1d56ae3b720da3d0236864e5c0dff9
Jacob Yamaki <jake.yamaki@gmail.com>
2014-10-04 13:57:21 -0400

    added firstrun flag
    disables unknown devices only with explicit user permission, keeps input
    devices from being disabled when their use is required for interaction

--- README.md
+++ README.md
@@ -6,23 +6,38 @@ USB device management to prevent against BadUSB attacks (Python 2.7)
 usbwatchdog.py
 --------------
 Detect unknown USB devices: present the user the device identifiers and have them allow/reject the drive
+
 Create an (un)trusted device store to recognize commonly used devices (on a per user basis)
 
 USBDeview.cfg
 -------------
-Config file for USBDeview, adds the required command needed to launch usbwatchdog
+Config file for USBDeview, adds the required command needed to launch usbwatchdog.py
+
+usbwatchdog-1.1.zip
+-------------------
+py2exe compiled script, unzip and drop the architecture specific version of USBDeview.
+
+usb.ids ver 2014.08.25, usbdeview.cfg provided within archive
+
+Config file has been updated to launch usbwatchdog.exe instead of .py
 
 Prerequisites
 -------------
 USBDeview - http://www.nirsoft.net/utils/usb_devices_view.html
-This must be architecture specifc, otherwise you will be unable to (de)activate USB devices
+
+This must be architecture specific, otherwise you will be unable to (de)activate USB devices
 
 usb.ids - http://www.linux-usb.org/usb.ids
+
 For additional manufacturer info and device names
 
-First-Run Warning
------------------
-Because unknown devices are automatically disabled, you may want to have an on-screen or secondary keyboard ready for when your USB keyboard is disabled.
+Installation
+------------
+Copy usbwatchdog.py, USBDeview.exe, USBDeview.cfg, and usb.ids into the same folder.
+
+The config file will be suitable if you have added python to your system path, else you need to edit it to reflect python's location.
+
+Because USBDeview.exe needs Administrator privileges to enable and disable devices, usbwatchdog.py also needs to be ran as administrator.
 
 Issues
 ------
--- usbwatchdog.py
+++ usbwatchdog.py
@@ -1,8 +1,12 @@
+# @author b6938236
+# @date 2014.10.04
+# @version 1.1
+
 import sys, os
-import xml.etree.ElementTree #for py2exe, otherwise not needed
+#import xml.etree.ElementTree #for py2exe, otherwise not needed
 import xml.etree.cElementTree as ET
 		
-def checkdevice(vid,pid,device_name,device_type,service_name,vendor_name,product_name):
+def checkdevice(vid,pid,device_name,device_type,service_name,vendor_name,product_name,disablefirst):
 	#build string, because instance ID is not provided by the program
 	usb_instance = str(vid) + ";"
 	usb_instance += str(pid) + ";"
@@ -26,16 +30,17 @@ def checkdevice(vid,pid,device_name,device_type,service_name,vendor_name,product
 				return
 	db_devices.close()
 		
-	#It's an unknown device, so disable it immediately
-	os.system("USBDeview.exe /disable_by_pid {0};{1}".format(vid,pid))
+	if (disablefirst):
+		os.system("USBDeview.exe /disable_by_pid {0};{1}".format(vid,pid))
 	print "New USB device identifies itself as \"{0}\"".format(device_name)
 	print "This device identifies as a {0} device".format(device_type)
 	print "usb.ids identifies this device as a {0} from {1}".format(product_name,vendor_name)
 	response = raw_input("Do you want to (A)ccept or (R)eject this device?")
 	response = response[:1].lower()
-	if response == "a":
-		#User accepted the device, so reconnect it
+	if (response == "a" and disablefirst): #device was disabled, user wants it back
 		os.system("USBDeview.exe /enable_by_pid {0};{1}".format(vid,pid))
+	if (response == "r" and not disablefirst): #user doesn't want device and it wasn't disabled
+		os.system("USBDeview.exe /disable_by_pid {0};{1}".format(vid,pid))
 	permanent = raw_input("Do you want to automatically apply this decision to this device in the future? (Yes/No)")
 	permanent = permanent[:1].lower()
 	if permanent == "y": #store the device string in the database
@@ -47,14 +52,18 @@ def checkdevice(vid,pid,device_name,device_type,service_name,vendor_name,product
 		db_devices.write(usb_instance)
 		db_devices.close()
 	
-def sweep():
+def sweep(disablefirst):
 	#generate XML report of all currently connected devices, then check each one
 	os.system("USBDeview.exe /sxml sweep.xml /DisplayDisconnected 0")
 	tree = ET.parse("sweep.xml")
 	root = tree.getroot()
 	usbs = []
-	for leaf in root:
-		checkdevice(leaf[11].text,leaf[12].text,leaf[0].text,leaf[2].text,leaf[22].text,leaf[19].text,leaf[20].text)
+	if (disablefirst):
+		for leaf in root:
+			checkdevice(leaf[11].text,leaf[12].text,leaf[0].text,leaf[2].text,leaf[22].text,leaf[19].text,leaf[20].text, True)
+	else:
+		for leaf in root:
+			checkdevice(leaf[11].text,leaf[12].text,leaf[0].text,leaf[2].text,leaf[22].text,leaf[19].text,leaf[20].text, False)
 		
 def man():
 	print "Prevents against unwanted or malicious USB devices"
@@ -76,7 +85,11 @@ def man():
 	print
 	print "USBWatchdog.exe --sweep"
 	print "     Checks all currently connected USB devices against the database"
+	print "     Will disable unknown devices first, then reactivate after user allows them"
 	print
+	print "USBWatchdog.exe --firstrun"
+	print "     Same as --sweep, but asks user about device before disabling them"
+	print "     Used to generate trusted database and keeps input devices from being disabled"
 	print
 	print "USB Watchdog is provided by b6938236"
 	print "https://github.com/b6938236"
@@ -89,9 +102,11 @@ def man():
 
 if len(sys.argv) == 1:
 	man()
-elif sys.argv[1] == "--sweep":
-	sweep()
-elif sys.argv[1] == "--device":
-	checkdevice(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7], sys.argv[8])
+elif sys.argv[1] == "--sweep": #Disable unknown devices first, then re-enable them after user OK
+	sweep(True) 
+elif sys.argv[1] == "--firstrun": #Disable unknown devices after user has rejected them
+	sweep(False) 
+elif sys.argv[1] == "--device": #New device has been inserted, USBDeview is passing along details
+	checkdevice(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7], sys.argv[8],True)
 else:
 	man()
